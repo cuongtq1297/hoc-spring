597746000d9ed21d08df054403a124ee86fb784e
trancuong121 <trancuongrich@gmail.com>
2023-02-09 22:06:20 +0700

    Revert "first commit"
    This reverts commit e6e6b329fc7ddde2b86c81282555828dfe35899a.

--- src/main/java/com/example/resfulapi/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.resfulapi.exception;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message){
-        super(message);
-    }
-}
